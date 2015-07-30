3051c658c0871f5ad27ae0e3801e4de2c4ff70b7
Mark Brough <mark.brough@gmail.com>
2015-07-30 17:51:19 +0200

    Move basic nav functions out of index.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
+  $(".nav-slider li a").click(function(e){
+    e.preventDefault();
+    var aHref = $(this).attr('href');
+    var aTag = $("article"+ aHref +"");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+    document.location.hash = aHref;
+  });
+  $('body').scrollspy({ target: '#main' });
+});
 
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('brand-shrink');
+  } else {
+    $('nav').removeClass('brand-shrink');
+  }
+});
