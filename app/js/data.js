cc101c070c4999a37fab0d43958935a61e71ccae
Mark Brough <mark.brough@gmail.com>
2015-08-24 18:41:38 +0200

    Sort companies by name

--- app/js/data.js
+++ app/js/data.js
@@ -219,8 +219,12 @@ makeCompaniesData = function(data) {
         return obj;
       }
     );
-    
 
+    this.data.sort(function (a,b) {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
 
   var _d = {};
   _d.data = this.data;
