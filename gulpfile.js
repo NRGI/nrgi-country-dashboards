ee636a1b4d78157561721279539e0605ba67c454
Mark Brough <mark.brough@gmail.com>
2015-08-27 18:01:15 +0200

    Add basic build process to take datapackages and combine them

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var sass = require('gulp-ruby-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var ghPages = require('gulp-gh-pages');
+var jsoncombine = require("gulp-jsoncombine");
+var jeditor = require("gulp-json-editor");
+var streamify = require("gulp-streamify");
 
 // Copy files
 gulp.task('copy_files', function() {
@@ -19,16 +22,27 @@ gulp.task('styles', function() {
     .pipe(reload({ stream:true }));
 });
 
+// Combine JSON files together
+gulp.task('combinedata', function() {
+  dataCombiner(["source/gdp.json", "source/extractives.json"],
+               "gdp-extractives-combined.json")
+  dataCombiner(["source/government-debt.json", "source/petroleum-account.json"],
+               "debt-petroleum.json")
+  dataCombiner(["source/government-revenues.json", "source/government-expenditure.json",
+    "source/extractives-revenues.json"],
+               "govt-revenues-expenditure-extractives.json")
+});
+
 gulp.task('gh-deploy', function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
 
 // Build task
-gulp.task('build', ['copy_files', 'styles']);
+gulp.task('build', ['copy_files', 'styles', 'combinedata']);
 
 // Watch files for changes and reload
-gulp.task('serve', ['copy_files', 'styles'], function() {
+gulp.task('serve', ['copy_files', 'styles', 'combinedata'], function() {
   browserSync({
     server: {
       baseDir: './dist'
@@ -41,3 +55,21 @@ gulp.task('serve', ['copy_files', 'styles'], function() {
 });
 
 gulp.task('deploy', ['build', 'gh-deploy']);
+
+// Read in various JSON files and combine them to an output file
+var dataCombiner = function(source_files, output_file) {
+  return gulp.src(source_files)
+    .pipe(jsoncombine(output_file, function(data){
+      return new Buffer (JSON.stringify(data));
+    }))
+    .pipe(streamify(jeditor(function(json) {
+      var out = {}
+      out.records = []
+      for (k in json) {
+        out.result = json[k].result;
+        out.records = out.records.concat(json[k].records);
+      }
+      return out;
+    })))
+    .pipe(gulp.dest("./app/data"));
+}
