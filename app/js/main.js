20da2919d6f6164b9875ee7518073e12ee386ad8
Mark Brough <mark.brough@gmail.com>
2015-09-16 21:39:18 +0200

    Update hash on scroll, move home map to main.js

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
     document.location.hash = aHref;
   })
 });
-
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
     $('nav').addClass('brand-shrink');
@@ -23,3 +22,26 @@ $(window).scroll(function() {
     $('nav').removeClass('brand-shrink');
   }
 });
+$(window).on('activate.bs.scrollspy', function(e) {
+  var $hash, $node;
+  $hash = $("a[href^='#']", e.target).attr("href").replace(/^#/, '');
+  $node = $('#' + $hash);
+  if ($node.length) {
+    $node.attr('id', '');
+  }
+  document.location.hash = $hash;
+  if ($node.length) {
+    return $node.attr('id', $hash);
+  }
+});
+layer_MapBox = new L.tileLayer(
+'https://d.tiles.mapbox.com/v3/markbrough.nf87l3dn/{z}/{x}/{y}.png',{
+    maxZoom: 18, attribution: 'MapBox Streets'
+})
+var homeMap = new L.Map("homeMap", {
+    center: new L.LatLng(5.7,-1),
+    zoom: 8,
+    maxZoom: 15
+});
+layer_MapBox.addTo(homeMap);
+homeMap.scrollWheelZoom.disable();
